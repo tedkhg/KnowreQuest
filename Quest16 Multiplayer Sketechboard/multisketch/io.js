42c2b35274107a39d16eb9a246f5aa4c98bb1ca3
Honggi Kim <tedkh@gmail.com>
2015-02-09 17:02:42 +0900

    Uploaded files for quest16

---
+++ Quest16 Multiplayer Sketechboard/multisketch/io.js
@@ -0,0 +1,32 @@
+var io = require('socket.io')();
+
+io.on('connection', function(socket) {
+	console.log("a user connected.");
+	socket.broadcast.emit('new user');
+
+	socket.on('disconnect', function() {
+		console.log("user disconnected.");
+	})
+
+	socket.on('create', function(cid,type) {
+		console.log(cid);
+		console.log(type);
+		socket.broadcast.emit('create', cid, type);
+	});
+
+	socket.on('init', function(cid) {
+		console.log('get id:' + cid);
+		socket.broadcast.emit('init', cid);
+	})
+
+	socket.on('move', function(cid, type) {
+		socket.broadcast.emit('move', cid, type);
+	})
+
+	socket.on('mouse', function(cid, dl, dt, dw, dh) {
+		socket.broadcast.emit('mouse', cid, dl, dt, dw, dh);
+	})
+})
+
+module.exports = io;
+// module.exports.curr = currentUser;
